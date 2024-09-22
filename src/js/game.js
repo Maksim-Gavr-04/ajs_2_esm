54376763654d8baf13f57f5134579938ab20b95b
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-09-22 14:23:06 +0700

    first commit

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export default Game;
+
+export class GameSavingData {
+
+}
+
+export function readGameSaving() {
+
+}
+
+export function writeGameSaving() {
+
+}
